1c5168122a4154d4ef58124420b8827dcab396db
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-02 12:51:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+  }
+  
+function happyHolidaysTo(string){
+  var name = "Maggie";
+    console.log(`Happy holidays to ${name}`);
+  }
+  
+function happyCustomHolidayTo(string){
+  var name    = "Maggie";
+  var holiday = "Christmas";
+    return console.log(`Happy ${holiday}, ${name}`);
+}
+
+function holidayCountdown(string){
+  var holiday = "Cinco de Mayo";
+  var days    = "34";
+    console.log(`It's ${days} until ${holiday}`);
+}
