82bd8a3f0bc46055df29cbec6c3f1b964ca46736
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-02 13:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name){
   }
   
 function happyCustomHolidayTo(holiday, name){
-    console.log(`Happy ${holiday}, ${name}`);
+    return `Happy ${holiday}, ${name}`
 }
 
 function holidayCountdown(string){
